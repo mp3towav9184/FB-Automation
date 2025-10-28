4260c7d7f7e797ef9a3788cc24e5d151767edd4a
zarir94 <shahriar@gmail.com>
2025-10-28 10:02:08 -0700

    update

--- pingRDP.py
+++ pingRDP.py
@@ -1,7 +1,10 @@
 import codehooks as chks
-import traceback
+import traceback, os, subprocess
 from time import sleep, time
 
+HOST = os.getenv('RDP_HOST')
+USER = os.getenv('RDP_USER')
+PASS = os.getenv('RDP_PASS')
 
 def needtoRun():
     try:
@@ -21,5 +24,29 @@ def needtoRun():
         return needtoRun()
 
 
+def connectRDP():
+    cmd = ["xfreerdp", f"/v:{HOST}", f"/u:{USER}", f"/p:{PASS}", "/size:1920x1080", "/smart-sizing", "/cert:ignore"]
+    try:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        try:
+            out, err = proc.communicate(timeout=80)
+            print("Process Finished Early!", flush=True)
+        except subprocess.TimeoutExpired:
+            print("Terminating Process...", flush=True)
+            proc.kill()
+            out, err = proc.communicate()
+
+        if out: print('='*20, 'OUTPUT', '='*20, f'\n{out}')
+        if err: print('='*20, 'ERROR', '='*20, f'\n{err}')
+    except:
+        traceback.print_exc()
+        print('Could not run connectRDP function', flush=True)
 
 
+def main():
+    if needtoRun():
+        print('Running connectRDP function...\n', flush=True)
+        connectRDP()
+
+if __name__ == '__main__':
+    main()
